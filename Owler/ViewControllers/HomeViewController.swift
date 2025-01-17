d3884972a0245c60b3ab171d29b806971f916916
Álvaro Perera <alvprodz@gmail.com>
2025-01-17 05:13:11 +0100

    20250117_APR_PostCreationScreenAdded

--- Owler/ViewControllers/HomeViewController.swift
+++ Owler/ViewControllers/HomeViewController.swift
@@ -8,4 +8,11 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        self.navigationItem.leftBarButtonItem = nil
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
 }
